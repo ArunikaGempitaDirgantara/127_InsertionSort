df905eb7d08a9133280ec0b85ff5fdefb2d41c24
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-15 11:14:51 +0700

    membuat fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -6,6 +6,27 @@ using namespace std;
 int n;
 int arr[11];
 
+void input() { //procedur untuk input
+	while (true) { //looping
+		cout << "Masukkan banyaknya elemen pada array : "; //Output ke layar
+		cin >> n; //Input dari pengguna
+		if (n <= 20) //Jika n kurang dari sama dengan 20
+			break; //Keluar dari loop
+		else { //Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n"; //Output ke layar
+		}
+	}
+	cout << endl; //Output baris kosong
+	cout << "==========" << endl; //Output ke layar
+	cout << "Masukkan elemen array" << endl; //Output ke layar
+	cout << "==========" << endl; //Output ke layar
+
+	for (int i = 0; i < n; i++) { //Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
+		cin >> a[i]; //Input dari pengguna
+	}
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
